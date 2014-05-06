e315fb19ef31d6f635fcd090291878ac9b1e4bff
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-06 10:28:22 -0400

    Forward (what should be no) output to /dev/null in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ pushd $(dirname $0) > /dev/null
 
 pkill tactique_proxy
 GOPATH=$(pwd) go build -o tactique_proxy proxy.go
-./tactique_proxy -logpath=$(pwd)/proxy.log &
+./tactique_proxy -logpath=$(pwd)/proxy.log > /dev/null &
 
 popd > /dev/null
