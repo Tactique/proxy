1ada88580b027e8abd2c476d7f45170d6f371bb8
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-07 23:32:45 -0400

    Deps noop and usage help with main

--- setup.sh
+++ setup.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-export GOPATH=$(pwd)
-go get
-go install github.com/mattn/go-sqlite3/
+ME=$(basename $0)
+
+function usage() {
+	echo "Usage: ${ME} [deps]"
+}
+
+function go_get_install() {
+	export GOPATH=$(pwd)
+	go get
+	go install github.com/mattn/go-sqlite3/
+}
+
+function main() {
+	if [ $# -lt 1 ]; then
+		go_get_install
+	else
+		if [ $1 == 'deps' ]; then
+			echo "no deps, ok"
+		else
+			usage
+		fi
+	fi
+}
+
+main $@
